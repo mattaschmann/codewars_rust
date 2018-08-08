8fff2fcec4d675757949f562851080eaa9fc1625
Matt Aschmann <maschmann@contractor.usgs.gov>
2018-08-08 15:50:10 -0600

    Fix erroneous memoization

--- src/dec_to_factorial_and_back.rs
+++ src/dec_to_factorial_and_back.rs
@@ -3,23 +3,25 @@
 use std::collections::HashMap;
 use std::char;
 
-const ASCII_ARRAY: [char; 36] = ['0','1','2','3','4','5','6','7','8','9',
-                                 'A','B','C','D','E','F','G','H','I','J',
-                                 'J','L','M','N','O','P','Q','R','S','T',
-                                 'U','V','W','X','Y','Z'];
+const ASCII_ARRAY: [char; 36] = [
+  '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H',
+  'I','J','J','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
 
-fn factorial(n: u64, map: &HashMap<u64, u64>) -> u64 {
+fn factorial(n: u64, map: &mut HashMap<u64, u64>) -> u64 {
   if n < 2 { return 1 }
 
-  match map.get(&n) {
-    Some(x) => x.clone(),
-    _ => {
-      n * factorial(n-1, map)
+  match map.contains_key(&n) {
+    true => *map.get(&n).unwrap(),
+    false => {
+      let cur = n * factorial(n-1, map);
+      map.insert(n, cur);
+      cur
     }
   }
 }
 
-fn largest_digit(n: u64, map: &HashMap<u64, u64>) -> u64 {
+fn largest_digit(n: u64, map: &mut HashMap<u64, u64>) -> u64 {
   let mut d = 1;
   while factorial(d, map) < n { d += 1 }
 
@@ -27,17 +29,14 @@ fn largest_digit(n: u64, map: &HashMap<u64, u64>) -> u64 {
 }
 
 fn dec2_fact_string(nb: u64) -> String {
-  println!("{}", nb);
-  let map = HashMap::new();
+  let mut map = HashMap::new();
 
-  let d = largest_digit(nb, &map);
+  let d = largest_digit(nb, &mut map);
   let mut result = String::new();
   let mut rem = nb;
   for i in (0..d+1).rev() {
-    let f = factorial(i, &map);
-    println!("{}", rem / f);
+    let f = factorial(i, &mut map);
     let c = ASCII_ARRAY[(rem / f) as usize];
-    println!("{}", c);
     result.push(c);
     rem %= f;
   }
@@ -46,12 +45,11 @@ fn dec2_fact_string(nb: u64) -> String {
 }
 
 fn fact_string_2dec(s: String) -> u64 {
-  println!("{}", s);
-  let map = HashMap::new();
+  let mut map = HashMap::new();
   s.chars()
    .rev()
    .enumerate()
-   .map(|(i, c)| factorial(i as u64, &map) * ASCII_ARRAY.iter().position(|&x| c == x).unwrap() as u64)
+   .map(|(i, c)| factorial(i as u64, &mut map) * ASCII_ARRAY.iter().position(|&x| c == x).unwrap() as u64)
    .sum()
 }
 
@@ -62,17 +60,17 @@ mod tests {
 
   #[test]
   fn test_factorial() {
-    let map = HashMap::new();
-    assert_eq!(factorial(0, &map), 1);
-    assert_eq!(factorial(1, &map), 1);
-    assert_eq!(factorial(2, &map), 2);
-    assert_eq!(factorial(6, &map), 720);
+    let mut map = HashMap::new();
+    assert_eq!(factorial(0, &mut map), 1);
+    assert_eq!(factorial(1, &mut map), 1);
+    assert_eq!(factorial(2, &mut map), 2);
+    assert_eq!(factorial(6, &mut map), 720);
   }
 
   #[test]
   fn test_largest_digit() {
-    let map = HashMap::new();
-    assert_eq!(largest_digit(730, &map), 6);
+    let mut map = HashMap::new();
+    assert_eq!(largest_digit(730, &mut map), 6);
   }
 
   #[test]
