d0cc402cb28a09043f6806da8b2ceb7779c98104
Matt Aschmann <maschmann@contractor.usgs.gov>
2018-08-08 13:53:06 -0600

    Prototype dec2_fact_string

--- src/dec_to_factorial_and_back.rs
+++ src/dec_to_factorial_and_back.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::char;
 
 fn factorial(n: u64, map: &HashMap<u64, u64>) -> u64 {
   if n < 2 { return 1 }
@@ -11,9 +12,28 @@ fn factorial(n: u64, map: &HashMap<u64, u64>) -> u64 {
   }
 }
 
-// fn dec2_fact_string(nb: u64) -> String {
-//   // your code
-// }
+fn largest_digit(n: u64, map: &HashMap<u64, u64>) -> u64 {
+  let mut d = 1;
+  while factorial(d, map) < n { d += 1 }
+
+  d-1
+}
+
+fn dec2_fact_string(nb: u64) -> String {
+  let map = HashMap::new();
+
+  let d = largest_digit(nb, &map);
+  let mut result = String::new();
+  let mut rem = nb;
+  for i in (0..d+1).rev() {
+    let f = factorial(i, &map);
+    let c = char::from_digit((rem / f) as u32, 10).unwrap();
+    result.push(c);
+    rem %= f;
+  }
+
+  result
+}
 
 // fn fact_string_2dec(s: String) -> u64 {
 //   // your code
@@ -31,5 +51,17 @@ mod tests {
     assert_eq!(factorial(1, &map), 1);
     assert_eq!(factorial(2, &map), 2);
     assert_eq!(factorial(6, &map), 720);
+  }
+
+  #[test]
+  fn test_largest_digit() {
+    let map = HashMap::new();
+    assert_eq!(largest_digit(730, &map), 6);
+  }
+
+  #[test]
+  fn test_dec2_fact_string() {
+    assert_eq!(dec2_fact_string(2982), "4041000");
+    assert_eq!(dec2_fact_string(463), "341010");
   }
 }
