6572bc8d9a588e5474bb33e31a18b31822519daa
Matt Aschmann <maschmann@contractor.usgs.gov>
2018-08-13 15:52:08 -0600

    Final solution

--- src/diophantine_equation.rs
+++ src/diophantine_equation.rs
@@ -1,33 +1,20 @@
 // https://www.codewars.com/kata/diophantine-equation
 
 fn solequa(n: u64) -> Vec<(u64, u64)> {
-  println!("n: {}", n);
-  let mut res = Vec::new();;
-  let mut i = match n % 2 {
-    0 => n + 1,
-    _ => n
-  };
 
-  while i > 1 {
-    let x = (i as f64 / 2_f64).ceil();
-    let y = (i as f64 / 4_f64).floor();
+  (1..(n as f64).sqrt().ceil() as u64 + 1)
+    .filter(|i| n % i == 0)
+    .fold(Vec::new(), |mut acc, i| {
+      let j = n/i;
+      if i > j { return acc }
 
-    // break early if there are no other possible solutions
-    if (x as u64).pow(2) < n { break }
+      let y = (j-i)/4;
+      let x = i+2*y;
 
-    for j in (0..(y as u64)+1).rev() {
-      let z = (x as u64).pow(2) - 4*(j).pow(2);
-      if z == n {
-        res.push((x as u64, j));
-        break;
-      }
-      if z > n { break }
-    }
+      if j == x+2*y && i == x-2*y { acc.push((x, y)) }
 
-    if i > 1 { i -= 2 }
-  }
-
-  res
+      acc
+    })
 }
 
 
@@ -51,6 +38,7 @@ mod tests {
     testing(9009, vec![(4505, 2252), (1503, 750), (647, 320), (505, 248), (415, 202), (353, 170), (225, 102), (153, 60), (135, 48), (103, 20), (97, 10), (95, 2)]);
     testing(90005, vec![(45003, 22501), (9003, 4499), (981, 467), (309, 37)]);
     testing(90002, vec![]);
-    testing(9000001, vec![]);
+    testing(9000001, vec![(4500001, 2250000), (73801, 36870)]);
   }
+
 }
