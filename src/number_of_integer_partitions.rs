6d16ee069cc4b80f824f73adc65bacb87af5fc10
Matt Aschmann <maschmann@contractor.usgs.gov>
2018-08-29 07:28:53 -0600

    Solution for number_of_integer_partitions

--- src/number_of_integer_partitions.rs
+++ src/number_of_integer_partitions.rs
@@ -1,35 +1,35 @@
 // https://www.codewars.com/kata/number-of-integer-partitions
 
+use std::collections::HashSet;
+
 fn partitions(n: isize) -> isize {
   let mut cur = vec![n];
-  let mut count = 1;
+
+  let mut seen = HashSet::new();
 
   while !cur.is_empty() {
-    println!("{:?}", cur);
     if cur.last().unwrap() == &1 {
       cur.pop().unwrap();
       continue;
     }
+    seen.insert(cur.clone());
 
     let tmp = cur.pop().unwrap() - 1;
     cur.push(tmp);
 
-    if cur.len() == 1 {
-      let mut rem = n - tmp;
-      let first = cur[0];
-      while rem > first {
-        cur.push(first);
-        rem -= first;
+    if !seen.contains(&cur) {
+      let mut rem = n - cur.iter().sum::<isize>();
+      let last = *cur.last().unwrap();
+      while rem > last {
+        cur.push(last);
+        rem -= last;
       }
       cur.push(rem);
     }
-
-    println!("{}", "count");
-    count += 1;
   }
 
-  println!("{}", "done");
-  count
+  println!("{:?}", seen);
+  seen.len() as isize + 1
 }
 
 
@@ -39,12 +39,11 @@ mod tests {
 
   #[test]
   fn test_partitions() {
-    // assert_eq!(partitions(1), 1);
-    // assert_eq!(partitions(2), 2);
-    // assert_eq!(partitions(3), 3);
-    // assert_eq!(partitions(4), 5);
-    // assert_eq!(partitions(5), 7);
-    // assert_eq!(partitions(7), 14);
+    assert_eq!(partitions(1), 1);
+    assert_eq!(partitions(2), 2);
+    assert_eq!(partitions(3), 3);
+    assert_eq!(partitions(4), 5);
+    assert_eq!(partitions(5), 7);
     assert_eq!(partitions(10), 42);
   }
 }
