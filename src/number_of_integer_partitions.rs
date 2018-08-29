bd785ae6fa17704e0217c238d10ef0e0c02247f1
Matt Aschmann <maschmann@contractor.usgs.gov>
2018-08-29 07:46:16 -0600

    Optimizations to number_of_integer_partitions

--- src/number_of_integer_partitions.rs
+++ src/number_of_integer_partitions.rs
@@ -3,32 +3,32 @@
 use std::collections::HashSet;
 
 fn partitions(n: isize) -> isize {
-  let mut cur = vec![n];
+  if n == 1 { return 1 }
 
+  let mut cur = vec![n];
   let mut seen = HashSet::new();
 
   while !cur.is_empty() {
-    if cur.last().unwrap() == &1 {
-      cur.pop().unwrap();
-      continue;
-    }
     seen.insert(cur.clone());
 
     let tmp = cur.pop().unwrap() - 1;
+    if tmp == 1 { continue; }
     cur.push(tmp);
 
     if !seen.contains(&cur) {
       let mut rem = n - cur.iter().sum::<isize>();
+      if rem == 1 { continue }
+
       let last = *cur.last().unwrap();
       while rem > last {
         cur.push(last);
         rem -= last;
       }
+      if rem == 1 { continue }
       cur.push(rem);
     }
   }
 
-  println!("{:?}", seen);
   seen.len() as isize + 1
 }
 
@@ -45,5 +45,6 @@ mod tests {
     assert_eq!(partitions(4), 5);
     assert_eq!(partitions(5), 7);
     assert_eq!(partitions(10), 42);
+    assert_eq!(partitions(100), 42);
   }
 }
