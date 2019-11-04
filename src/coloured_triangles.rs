95295e44abe7732f5d10b06f8537f0d8f44e0d94
Matt Aschmann <matt.aschmann@gmail.com>
2019-11-04 12:29:28 -0700

    Final solution for coloured_triangles

--- src/coloured_triangles.rs
+++ src/coloured_triangles.rs
@@ -1,24 +1,21 @@
 // https://www.codewars.com/kata/5a25ac6ac5e284cfbe000111
 
 fn triangle(row_str: &str) -> String {
-  if row_str.len() <= 1 { return row_str.to_string() };
+  if row_str.len() <= 1 {
+    return row_str.to_string();
+  };
 
-  let mut res = String::new();
-
-  for i in 0..row_str.len()-1 {
-    let snip = row_str.get(i..i+2);
-
-    match snip {
-      Some("GG") => res.push('G'),
-      Some("RR") => res.push('R'),
-      Some("BB") => res.push('B'),
-      Some("BG") | Some("GB") => res.push('R'),
-      Some("RG") | Some("GR") => res.push('B'),
-      Some("BR") | Some("RB") => res.push('G'),
-      _ => ()
-    }
-
-  }
+  let res = row_str
+    .chars()
+    .collect::<Vec<char>>()
+    .windows(2)
+    .map(|x| match x {
+      ['R', 'R'] | ['B', 'G'] | ['G', 'B'] => 'R',
+      ['B', 'B'] | ['R', 'G'] | ['G', 'R'] => 'B',
+      ['G', 'G'] | ['B', 'R'] | ['R', 'B'] => 'G',
+      _ => panic!("Combination not found!"),
+    })
+    .collect::<String>();
 
   triangle(&res)
 }
@@ -28,16 +25,16 @@ fn triangle(row_str: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_triangle() {
-        assert_eq!(triangle("B"), "B");
-        assert_eq!(triangle("GB"), "R");
-        assert_eq!(triangle("RRR"), "R");
-        assert_eq!(triangle("RGBG"), "B");
-        assert_eq!(triangle("RBRGBRB"), "G");
-        assert_eq!(triangle("RBRGBRBGGRRRBGBBBGG"), "G");
-        assert_eq!(triangle("GB"), "R");
-    }
+  #[test]
+  fn test_triangle() {
+    assert_eq!(triangle("B"), "B");
+    assert_eq!(triangle("GB"), "R");
+    assert_eq!(triangle("RRR"), "R");
+    assert_eq!(triangle("RGBG"), "B");
+    assert_eq!(triangle("RBRGBRB"), "G");
+    assert_eq!(triangle("RBRGBRBGGRRRBGBBBGG"), "G");
+    assert_eq!(triangle("GB"), "R");
+  }
 }
