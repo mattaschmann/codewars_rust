aa3c41b7c4bf2a580b3db08e53c989942f18d0e8
Matt Aschmann <matt.aschmann@gmail.com>
2019-11-05 15:41:21 -0700

    Final solution

--- src/target_date.rs
+++ src/target_date.rs
@@ -5,13 +5,12 @@ extern crate chrono;
 use self::chrono::{NaiveDate, Duration};
 
 fn date_nb_days(a0: i32, a: i32, p: i32) -> String {
-    // @Matt TODO: figure out the math
-
-    let mut current: f32 = a0 as f32;
+    let mut current: f64 = a0 as f64;
     let mut days = 0;
 
-    while current < a as f32 {
-      current = current + (current * p as f32 / 36000.0);
+    // This should be calculated using math
+    while current < a as f64 {
+      current += current * p as f64 / 36000.0;
       days += 1;
     }
 
@@ -42,5 +41,6 @@ fn date_nb_days(a0: i32, a: i32, p: i32) -> String {
         dotest(100, 150, 2, "2035-12-26");
         dotest(4281, 5087, 2, "2024-07-03");
         dotest(4620, 5188, 2, "2021-09-19");
+        dotest(258, 625, 1, "2103-03-19");
     }
 }
