68502a5ad650685ef495e0b1a9b7132a0583f3fc
Matt Aschmann <matt.aschmann@gmail.com>
2019-12-16 15:23:30 -0700

    Fix bug in prime calculation

--- src/gap_in_primes.rs
+++ src/gap_in_primes.rs
@@ -20,7 +20,7 @@ fn is_prime(n: u64) -> bool {
   if n % 2 == 0 || n % 3 == 0 { return false; }
 
   let mut i = 5;
-  while i * i < n {
+  while i * i <= n {
     if n % i == 0 || n % (i + 2) == 0 {
       return false;
     }
